d1ea0760e4b1bb6b0c45e4ffe965bff9a005adc7
Santosh2411 <SantoshBharugade95@gmail.com>
2022-12-13 22:50:14 +0530

    modified files

---
+++ demo01.java
@@ -0,0 +1,17 @@
+public class demo01
+{
+   void m1(int a)
+{
+  System.out.println("m1 method");
+}
+}
+ void m1()
+{
+System.out.println("m1 method");
+}
+}
+    public static void main(String args[])
+{
+       System.out.println("main method");
+}
+}
---
+++ demo02.java
@@ -0,0 +1,22 @@
+class demo02
+{
+void m1(int a)
+{
+  System.out.println("m1 method");
+}
+
+ void m1(int b,int c)
+{
+System.out.println("m1 method");
+}
+
+    public static void main(String args[])
+{
+       System.out.println("main method");
+}
+
+}
+
+
+
+}
